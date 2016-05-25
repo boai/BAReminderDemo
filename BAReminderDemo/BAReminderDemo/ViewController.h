1768c00827ed4f8ac97e127e632815c41143ccc2
博爱“git config --global user.name 博爱” <sunboyan@outlook.com>
2016-05-25 16:08:05 +0800

    框架完成

---
+++ BAReminderDemo/BAReminderDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BAReminderDemo
+//
+//  Created by 博爱 on 16/5/25.
+//  Copyright © 2016年 博爱之家. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BAReminderDemo/BAReminderDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BAReminderDemo
+//
+//  Created by 博爱 on 16/5/25.
+//  Copyright © 2016年 博爱之家. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
